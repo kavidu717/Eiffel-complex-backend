b05493d1564594e01178dfa540ef2c5c9c7cab20
kavidu717 <dushmanthakavidu143@gmail.com>
2026-05-20 00:44:16 +0530

    update the port

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ app.use("/api/v1/payment",paymentRouter
 
  
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
